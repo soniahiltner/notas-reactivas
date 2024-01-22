14c641c1bf1ab80290db6eb115aa40a7979f38b3
Sonia <sonia.hiltner@gmail.com>
2024-01-22 22:17:53 +0100

    Add protected route component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react"
-import "./App.css"
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home/Home"
+import { useState } from 'react'
+import './App.css'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
-import Notes from "./pages/Notes/Notes"
-import Header from "./components/Header/Header"
-import Todos from "./pages/Todos/Todos"
-import Topics from "./pages/Topics/Topics"
-import Topic from "./pages/Topic/Topic"
+import Notes from './pages/Notes/Notes'
+import Header from './components/Header/Header'
+import Todos from './pages/Todos/Todos'
+import Topics from './pages/Topics/Topics'
+import Topic from './pages/Topic/Topic'
+import { useAuth } from './context/AuthContext'
 
 function App() {
-  const [user, setUser] = useState('')
+  //const [user, setUser] = useState('')
+  const { user } = useAuth()
+
+  // Protected routes
+  const ProtectedRoute = ({ user, redirectPath = '/login', children }) => {
+    if (!user) {
+      return (
+        <Navigate
+          to={redirectPath}
+          replace
+        />
+      )
+    }
+    return children ? children : <Outlet />
+  }
 
   return (
     <div className='app'>
-      <Header user={user} />
+      <Header />
       <div className='container'>
         <Routes>
           <Route
@@ -30,23 +45,31 @@ function App() {
             path='/register'
             element={<Register />}
           />
+          <Route element={<ProtectedRoute user={user} />}>
+            <Route
+              path='/notes'
+              element={<Notes />}
+            />
+            <Route
+              path='/topics'
+              element={<Topics />}
+            />
+            <Route
+              path='/topics/:id'
+              element={<Topic />}
+            />
+            <Route
+              path='/todos'
+              element={<Todos />}
+            />
+          </Route>
           <Route
-            path='/notes'
-            element={<Notes />}
-          />
-          <Route
-            path='/topics'
-            element={<Topics />}
-          />
-          <Route path="/topics/:id" element={<Topic />} />
-          <Route
-            path='/todos'
-            element={<Todos />}
-          />
+            path='*'
+            element={<Navigate to='/' replace />} />
         </Routes>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
